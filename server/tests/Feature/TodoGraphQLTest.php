a7cacad7aabd422af5ec3254eaac6f1e04513e3b
kaminuma <>
2025-07-13 01:01:15 +0900

    バックエンドテストFix teste2eaのpackage.json追記

--- server/tests/Feature/TodoGraphQLTest.php
+++ server/tests/Feature/TodoGraphQLTest.php
@@ -14,6 +14,9 @@ class TodoGraphQLTest extends TestCase
     public function test_todo_crud_via_graphql()
     {
         $user = User::factory()->create();
+        
+        // ユーザーを認証
+        $this->actingAs($user);
 
         // 1. 作成
         $createMutation = [
